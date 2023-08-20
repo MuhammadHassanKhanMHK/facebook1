456988889fa031dd0f29b577f8e957ce88caa1b9
MuhammadHassanKhanMHK <96019352+MuhammadHassanKhanMHK@users.noreply.github.com>
2023-08-21 02:48:52 +0500

    FacebookApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import FacebookCard from './FacebookCard';
+import sampleImage from './sample image.jpg';
+import sampleImage2 from './123.jpg';
+import sampleImage3 from './456.jpg';
+import sampleImage4 from './789.jpg';
+
 
 function App() {
+  const user = {
+    name: 'Hassan Khan',
+    avatarUrl: sampleImage, 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      {/* <FacebookCard
+        user={user}
+        time="10 hr"
+        imageUrl={sampleImage}
+        postText="Captured a picture of Photographer."
+      /> */}
+      <FacebookCard
+        user={user}
+        time="4 hr"
+        imageUrl={sampleImage2}
+        postText="Have A Good Day!!!"
+      />
+      <FacebookCard
+        user={user}
+        time="15 hr"
+        imageUrl={sampleImage3}
+        postText="Grapes on Tree."
+      />
+      <FacebookCard
+        user={user}
+        time="14 hr"
+        imageUrl={sampleImage4}
+        postText="Creative Idea."
+      />
     </div>
   );
 }
